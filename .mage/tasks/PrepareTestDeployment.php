4a5582a15dd9576c87507d2195d1870e8b736cf4
estebanc <estebanc@121customerinsight.co.uk>
2015-02-16 15:19:43 +0000

    Creating specific files depend on the environment
    Changes in the mage test environment. Adding prepareTestDeplyment exec on deploy

---
+++ .mage/tasks/PrepareTestDeployment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Task;
+
+use Mage\Task\AbstractTask;
+
+class PrepareTestDeployment extends AbstractTask
+{
+    public function getName()
+    {
+        return 'Preparing the deployment on the test environment';
+    }
+
+    public function run()
+    {
+        $commandList = array(
+            'mv app/config/parameters.yml.test app/config/parameters.yml',
+            'rm -rf app/config/parameters.yml.*',
+            'mv web/app.php.test /var/www/fsb/web/app.php',
+            'rm -rf web/app.php.*'
+        );
+
+        $command = implode(" && ", $commandList);
+
+        $result = $this->runCommandRemote($command);
+
+        return $result;
+    }
+}
