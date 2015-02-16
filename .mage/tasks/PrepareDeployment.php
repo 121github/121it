6e1d2a8503fea0d4a711de83c4f8e5aca91775ae
estebanc <estebanc@121customerinsight.co.uk>
2015-02-16 15:40:13 +0000

    Creating specific files depend on the environment
    Adding conf files for production environments
    
    Changes in the mage test environment. Adding prepareAcceptDeplyment and prepareDeployment exec on deploy for accept and production environments

--- .mage/tasks/PrepareDeployment.php
+++ .mage/tasks/PrepareDeployment.php
@@ -4,19 +4,19 @@ namespace Task;
 
 use Mage\Task\AbstractTask;
 
-class PrepareTestDeployment extends AbstractTask
+class PrepareAcceptDeployment extends AbstractTask
 {
     public function getName()
     {
-        return 'Preparing the deployment on the test environment';
+        return 'Preparing the deployment on the production environment';
     }
 
     public function run()
     {
         $commandList = array(
-            'mv app/config/parameters.yml.test app/config/parameters.yml',
+            'mv app/config/parameters.yml.dist app/config/parameters.yml',
             'rm -rf app/config/parameters.yml.*',
-            'mv web/app.php.test web/app.php',
+            'mv web/app.php web/app.php',
             'rm -rf web/app.php.*'
         );
 
