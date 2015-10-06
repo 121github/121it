0d84177eafcc25185bc5d19f58edbe57e161ea9d
Esteban Correa <estebanc@121customerinsight.co.uk>
2015-10-06 15:37:04 +0100

    Centralize the uk_postcodes #121IT-19

--- src/It121/AddressBundle/EventListener/PafPostcodeListener.php
+++ src/It121/AddressBundle/EventListener/PafPostcodeListener.php
@@ -28,59 +28,9 @@ class PafPostcodeListener
     {
         $em = $eventArgs->getEntityManager();
         $uow = $em->getUnitOfWork();
-        $client = $this->PostcodeIoAPIService->get('box_uk_postcodes_io.client');
-
-        $postcodes = array();
+        //$client = $this->PostcodeIoAPIService->get('box_uk_postcodes_io.client');
 
         foreach ($uow->getScheduledEntityInsertions() as $entity) {
-//            if ($entity instanceof PafPostcode) {
-//
-//                if (!in_array($entity->getPostcode(), $postcodes)) {
-//                    array_push($postcodes, $entity->getPostcode());
-//                    $postcodeIo = new PostcodeIo();
-//
-//                    try{
-//                        $response = $client->lookup(array('postcode' => $entity->getPostcode()));
-//
-//                        $postcodeIo->setPostcode($response['result']['postcode']);
-//                        $postcodeIo->setQuality($response['result']['quality']);
-//                        $postcodeIo->setEastings($response['result']['eastings']);
-//                        $postcodeIo->setNorthings($response['result']['northings']);
-//                        $postcodeIo->setCountry($response['result']['country']);
-//                        $postcodeIo->setNhsHa($response['result']['nhs_ha']);
-//                        $postcodeIo->setLongitude($response['result']['longitude']);
-//                        $postcodeIo->setLatitude($response['result']['latitude']);
-//                        $postcodeIo->setParliamentaryConstituency($response['result']['parliamentary_constituency']);
-//                        $postcodeIo->setEuropeanElectoralRegion($response['result']['european_electoral_region']);
-//                        $postcodeIo->setPrimaryCareTrust($response['result']['primary_care_trust']);
-//                        $postcodeIo->setRegion($response['result']['region']);
-//                        $postcodeIo->setLsoa($response['result']['lsoa']);
-//                        $postcodeIo->setMsoa($response['result']['msoa']);
-//                        $postcodeIo->setIncode($response['result']['incode']);
-//                        $postcodeIo->setOutcode($response['result']['outcode']);
-//                        $postcodeIo->setAdminDistrict($response['result']['admin_district']);
-//                        $postcodeIo->setParish($response['result']['parish']);
-//                        $postcodeIo->setAdminCounty($response['result']['admin_county']);
-//                        $postcodeIo->setAdminWard($response['result']['admin_ward']);
-//                        $postcodeIo->setCcg($response['result']['ccg']);
-//                        $postcodeIo->setNuts($response['result']['nuts']);
-//
-//                        $em->persist($postcodeIo);
-//                        $logMetadata = $em->getClassMetadata("It121\AddressBundle\Entity\PostcodeIo");
-//                        $uow->computeChangeSet($logMetadata, $postcodeIo);
-//
-//                    } catch(\Exception $e){
-//
-//                    }
-//                }
-//
-//
-//                $entity->setPostcodeIo($postcodeIo);
-//
-//                $em->persist($entity);
-//                $classMetadata = $em->getClassMetadata(get_class($entity));
-//                $uow->recomputeSingleEntityChangeSet($classMetadata, $entity);
-//            }
         }
 
         foreach ($uow->getScheduledEntityUpdates() as $entity) {
